f6443d4777a1a0ecfcf47ec860796de479e40a5e
Shira Dusowitz <sdusowitz@gmail.om>
2016-04-13 22:33:43 -0400

    added sound to valid and invalid words

--- src/main/java/boggle/BoggleFrame.java
+++ src/main/java/boggle/BoggleFrame.java
@@ -699,9 +699,25 @@ public class BoggleFrame extends JFrame {
 	}
 
 	public void setWordInvalid() {
+		try {
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(new File(getClass().getResource(
+							"/wrongAnswerSound.wav").getFile()));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
 		Thread thread = new Thread() {
 			@Override
 			public void run() {
+
 				correctLabel.setIcon(xImage);
 				try {
 					Thread.sleep(1000);
@@ -715,6 +731,21 @@ public class BoggleFrame extends JFrame {
 	}
 
 	public void setWordValid() {
+		try {
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(new File(getClass().getResource(
+							"/rightAnswerSound.wav").getFile()));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
 		Thread thread = new Thread() {
 			@Override
 			public void run() {
